7a38d3dd90c08c8245056146d10dc3b3790016e4
Ben Rosa <defttouch.games@gmail.com>
2016-11-20 14:52:10 +0000

    Editing ajax request.

--- tp/js/grok-translate.js
+++ tp/js/grok-translate.js
@@ -2,7 +2,10 @@ var groktranslate = {
 	translate: function(formData) {
 		$.ajax({
 			url: "/translator/online_translation/reverso/spanish/english/",
-			data: formData
+			data: formData,
+			success: function(data) {
+				console.log(data);
+			}
 		});
 	}
 };
